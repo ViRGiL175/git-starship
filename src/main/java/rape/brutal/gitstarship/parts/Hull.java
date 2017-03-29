da012ad5658f7ee5dd81326a255561eab5c35977
Alvahaze <pisarevsc@gmail.com>
2017-03-29 16:33:17 +0400

    Adding constructors for parts of a ship
    - Added constructor for class ShipPart
    - Added constructor for class Engine
    - Added constructor for class Hull

--- src/main/java/rape/brutal/gitstarship/parts/Hull.java
+++ src/main/java/rape/brutal/gitstarship/parts/Hull.java
@@ -7,31 +7,57 @@ package rape.brutal.gitstarship.parts;
 
 import rape.brutal.gitstarship.ammunition.Gun;
 
+import java.util.Objects;
+import java.util.Scanner;
+
 /**
  * Created by haze on 22.03.2017.
  */
 public class Hull extends ShipPart {
 
-    private int cost;
-    private String name;
-    private int size;
+    private Gun[] guns;
 
-    private Gun[] guns = new Gun[8];
+    public Hull(int cost, int size, String name) {
+        super(cost, size, name);
+        this.guns = new Gun[8];
+    }
 
     public Gun[] getGuns() {
         return guns;
     }
 
-    public void setGuns(Gun[] guns) {
-        this.guns = guns;
+    public void addGun(Gun gun) {
+        boolean isGunReplacing = true;
+        for (int i = 0; i < 8; i++) {
+            if (guns[i] == null) {
+                isGunReplacing = false;
+                guns[i] = gun;
+                break;
+            }
+        }
+        if (isGunReplacing) {
+            System.out.println("Все отсеки заняты, хотите заменить пушку? (Введите 'y' или anykey)");
+            String inputString = new Scanner(System.in).nextLine();
+
+            if (Objects.equals(inputString, "y")) {
+                System.out.println("\tПушка\tУрон\tРазмер\tСтоимость");
+                for (int i = 0; i < 8; i++) {
+                    System.out.println("№" + i + "\t" + guns[i].getName() + "\t" +
+                            guns[i].getDamage() + "\t\t" + guns[i].getSize() + "\t\t" + guns[i].getCost());
+                }
+
+                System.out.println("Введите номер заменяемой пушки");
+                int inputNumber = new Scanner(System.in).nextInt();
+                removeGun(inputNumber);
+                guns[inputNumber] = gun;
+            }
+
+        }
+
     }
 
-    public void addGun() {
-
-    }
-
-    public void removeGun() {
-
+    public void removeGun(int gunNumber) {
+        guns[gunNumber] = null;
     }
 
     @Override
